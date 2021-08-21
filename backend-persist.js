829885fdcbd4297dca62e5d9cdd731abf6e61a6c
Louis Ross <louis.ross@gmail.com>
2021-08-21 11:06:29 -0700

    Working docker compose service stack

--- backend-persist.js
+++ backend-persist.js
@@ -5,8 +5,14 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const path = require('path');
 const net = require('net');
+const fs = require('fs');
 
 const cors = require("cors"); // enforce CORS, will be set to frontend URL when deployed
+
+let rawdata = fs.readFileSync('/configuration/configuration.json');
+let configuration = JSON.parse(rawdata);
+console.log(configuration);
+
 
 const cors_conf = {
   origin: ["http://0.0.0.0:5000"], // ! temporary
@@ -20,8 +26,9 @@ app.use(cors());
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
+const targetUrl = configuration.services.modelPersist.host + ':' + configuration.services.modelPersist.port;
 const proxyMiddleware = createProxyMiddleware({
-  target: 'http://192.168.1.150:5000',
+  target: targetUrl,
   changeOrigin: true,
 });
 
@@ -29,11 +36,11 @@ app.use(proxyMiddleware);
 
 var server = http.createServer(app);
 
-const PORT = 5001;
+const PORT = 5000;
 
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-server.listen(PORT, () => console.log(`Server running on port http://backend:${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port http://backend:${PORT}, proxying to ${targetUrl}`));
 
